251205e7a2b3c88bdd3f8fff4818e56e0674be8f
Erickfoxes <00092217@uca.edu.sv>
2019-04-09 08:18:35 -0600

    creando conexion

--- app/src/main/java/com/naldana/ejemplo10/utils/NetworkUtils.kt
+++ app/src/main/java/com/naldana/ejemplo10/utils/NetworkUtils.kt
@@ -1,4 +1,59 @@
 package com.naldana.ejemplo10.utils
 
+import android.net.Uri
+import android.util.Log
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
 class NetworkUtils {
+
+    class NetworkUtils {
+        val MONEDA_API_BASE_URL = "https://pokeapi.co/api/v2/"
+        val MONEDA_INFO = "moneda"
+        val MONEDA_TYPE = "type"
+
+        private val TAG = NetworkUtils::class.java.simpleName
+
+        fun buildUrl(root: String, pokeID: String): URL {
+            val builtUri = Uri.parse(MONEDA_API_BASE_URL)
+                .buildUpon()
+                .appendPath(root)
+                .appendPath(pokeID)
+                .build()
+
+            val url = try {
+                URL(builtUri.toString())
+            } catch (e: MalformedURLException) {
+                URL("")
+            }
+
+            Log.d(TAG, "Built URI $url")
+
+            return url
+        }
+
+        @Throws(IOException::class)
+        fun getResponseFromHttpUrl(url: URL): String {
+            val urlConnection = url.openConnection() as HttpURLConnection
+            try {
+                val `in` = urlConnection.inputStream
+
+                val scanner = Scanner(`in`)
+                scanner.useDelimiter("\\A")
+
+                val hasInput = scanner.hasNext()
+                return if (hasInput) {
+                    scanner.next()
+                } else {
+                    ""
+                }
+            } finally {
+                urlConnection.disconnect()
+            }
+        }
+    }
+
 }
