b11e99fa721d688c2ee12a8e86a6d9e16096b79b
Erickfoxes <00092217@uca.edu.sv>
2019-04-09 08:24:25 -0600

    Creacion de pakage models y dentro Moneda

---
+++ app/src/main/java/com/naldana/ejemplo10/models/Moneda.kt
@@ -0,0 +1,5 @@
+package com.naldana.ejemplo10.models
+
+data class Moneda(val name: String, val country: String, val Value: Number, val value_us:Number,val year: Number, val review: String, val isAvailable: Boolean, val img: String) {
+
+}
